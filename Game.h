ea796807206cc938b8dfc85b35600930613151cc
Seungje Choi <seungje@gmail.com>
2024-03-31 17:50:24 +0900

    초기 커밋.

---
+++ Game.h
@@ -0,0 +1,127 @@
+#pragma once
+#include <SDL.h>
+#include <string.h>
+
+constexpr int WINDOW_W = 800;
+constexpr int WINDOW_H = 600;
+
+constexpr int BLOCK_W = 5;
+constexpr int BLOCK_H = 5;
+
+constexpr int MAP_W = WINDOW_W / BLOCK_W;
+constexpr int MAP_H = WINDOW_H / BLOCK_H;
+
+constexpr int BLOCK_MAX = MAP_W * MAP_H;
+
+const char img[7][30]{ "assets/background.png",
+                       "assets/block_red_s.png",
+                       "assets/block_orange_s.png",
+                       "assets/block_yellow_s.png",
+                       "assets/block_green_s.png",
+                       "assets/block_blue_s.png",
+                       "assets/block_purple_s.png" };
+
+bool isRunning = true;
+
+int map[MAP_W][MAP_H]{};
+int Radius = 10;
+
+constexpr int FPS = 60;
+constexpr int frameDelay = 1000 / FPS;
+constexpr int updateCount = 1;
+
+Uint32 frameStart, frameTime, frameCount = 0;
+
+struct Block
+{
+    int x, y;
+    int blockIdx;
+    bool onScreen;
+    bool updatable;
+    SDL_Rect rect;
+    SDL_Texture* texture;
+
+} block[BLOCK_MAX];
+
+static int curBlockIdx = 0;
+
+void putBlock(int mousePosX, int mousePosY, int i, int j, int Radius, SDL_Texture* blockTexture)
+{
+    int x = mousePosX / BLOCK_W + i;
+    int y = mousePosY / BLOCK_H + j;
+
+    if (curBlockIdx < BLOCK_MAX && map[x][y] == 0 && !(x < 0) && !(y < 0) && (x < MAP_W) && (y < MAP_H))
+    {
+        map[x][y] = 1;
+        block[curBlockIdx].x = x;
+        block[curBlockIdx].y = y;
+        block[curBlockIdx].blockIdx = curBlockIdx;
+        block[curBlockIdx].onScreen = true;
+        block[curBlockIdx].updatable = true;
+        block[curBlockIdx].rect.x = block[curBlockIdx].x * BLOCK_W;
+        block[curBlockIdx].rect.y = block[curBlockIdx].y * BLOCK_H;
+        block[curBlockIdx].rect.w = BLOCK_W;
+        block[curBlockIdx].rect.h = BLOCK_H;
+        block[curBlockIdx].texture = blockTexture;
+
+        curBlockIdx++;
+    }
+}
+
+void putBlocks(int mousePosX, int mousePosY, int Radius, SDL_Texture* blockTexture)
+{
+    // In window area
+    if (!(mousePosX < 0) && !(mousePosY < 0) && (mousePosX < WINDOW_W) && (mousePosY < WINDOW_H))
+    {
+        for (int i = -Radius; i < Radius; i++)
+        {
+            int y = (int)sqrt(Radius * Radius - i * i);
+            for (int j = -y; j < y; j++)
+            {
+                putBlock(mousePosX, mousePosY, i, j, Radius, blockTexture);
+            }
+        }
+    }
+}
+
+void dropBlock(Block& block)
+{
+    map[block.x][block.y] = 0;
+    block.y++;
+    map[block.x][block.y] = 1;
+    block.rect.y += BLOCK_H;
+}
+
+void checkStop(Block& block)
+{
+    // on the block
+    if (map[block.x][block.y+1] == 1)
+    {
+        block.updatable = false;
+    }
+    else
+    {
+        block.updatable = true;
+    }
+    // on the bottom
+    if (block.y == MAP_H - 1)
+    {
+        block.updatable = false;
+    }
+}
+
+void clearBlock()
+{
+    for (int i = 0; i < curBlockIdx; i++)
+    {
+        block[curBlockIdx].onScreen = false;
+        block[curBlockIdx].updatable = false;
+    }
+    memset(map, 0, sizeof(int) * BLOCK_MAX);
+    curBlockIdx = 0;
+}
+
+void setBlockColor(SDL_Texture* &des, SDL_Texture* &src)
+{
+    des = src;
+}
---
+++ main.cpp
@@ -0,0 +1,144 @@
+﻿#include "Game.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+
+
+int main(int argc, char** argv)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window = SDL_CreateWindow(
+        "PixelDrop", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_W, WINDOW_H, false);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Event event;
+
+    // Texture
+    SDL_Surface* tmpSurface;
+
+    SDL_Texture* texture[7];
+    for (int i = 0; i < 7; i++)
+    {
+        tmpSurface = IMG_Load(img[i]);
+        texture[i] = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+    }
+    SDL_Texture* blockTexture = texture[4];
+
+    SDL_FreeSurface(tmpSurface);
+    // End of Texture
+
+    // Rectangle
+    SDL_Rect bgRect{ 0, 0, WINDOW_W, WINDOW_H };
+
+    // Main Loop
+    while (isRunning)
+    {
+        // Get Ticks
+        frameStart = (Uint32)SDL_GetTicks64();
+
+        // Get Event
+        SDL_PollEvent(&event);
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+
+        /* KEYDOWN event */
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym)
+            {
+            case SDLK_SPACE:
+                clearBlock();
+                break;
+            case SDLK_1:
+                setBlockColor(blockTexture, texture[1]);
+                break;
+            case SDLK_2:
+                setBlockColor(blockTexture, texture[2]);
+                break;
+            case SDLK_3:
+                setBlockColor(blockTexture, texture[3]);
+                break;
+            case SDLK_4:
+                setBlockColor(blockTexture, texture[4]);
+                break;
+            case SDLK_5:
+                setBlockColor(blockTexture, texture[5]);
+                break;
+            case SDLK_6:
+                setBlockColor(blockTexture, texture[6]);
+                break;
+            }
+            break;
+
+        /* Mouse event */
+        case SDL_MOUSEBUTTONDOWN:
+            switch (event.button.button)
+            {
+            case SDL_BUTTON_LEFT:
+                putBlocks(event.button.x, event.button.y, Radius, blockTexture);
+                break;
+            }
+            break;
+
+        case SDL_MOUSEMOTION:
+            switch (event.motion.state)
+            {
+            case SDL_BUTTON_LMASK:
+                putBlocks(event.motion.x, event.motion.y, Radius, blockTexture);
+                break;
+            }
+        }
+
+        // Update
+        if (frameCount % updateCount == 0)
+        {
+            for (int i = 0; i < curBlockIdx; i++)
+            {
+                checkStop(block[i]);
+                if (block[i].updatable)
+                {
+                    dropBlock(block[i]);
+                }
+            }
+        }
+
+        // Render
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture[0], NULL, NULL);
+        for (int i = 0; i < curBlockIdx; i++)
+        {
+            if (block[i].onScreen)
+            {
+                SDL_RenderCopy(renderer, block[i].texture, NULL, &block[i].rect);
+            }
+        }
+        SDL_RenderPresent(renderer);
+
+
+        // Control iteration time for Specific FPS
+        frameTime = (Uint32)SDL_GetTicks64() - frameStart;
+        if (frameDelay > frameTime)
+        {
+            SDL_Delay(frameDelay - frameTime);
+        }
+        if (frameTime > 0)
+        {
+            //printf("FPS : %d\n", 1000/((Uint32)SDL_GetTicks64() - frameStart));
+        }
+
+        frameCount++;
+    }
+
+    // Close SDL
+    for (int i = 0; i < 7; i++)
+    {
+        SDL_DestroyTexture(texture[i]);
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
